cd69de62009c57358ba3ffaa26c028b1aa54f232
Nikhil Khandavilli <137254164+greenhawks@users.noreply.github.com>
2024-02-29 23:41:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import React from "react"
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Java Developer", "Android Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 100,
+        loop: true
+    });
+});
